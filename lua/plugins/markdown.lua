085ba3f82bce9670cf156211a33763c046cc71b2
Aayush Maharjan <aayushmaharjan497@gmail.com>
2026-08-11 17:24:13 +0545

    viw leader

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    keys = {
+      {
+        "<leader>i",
+        'c*<C-r>"*<Esc>',
+        mode = "v",
+        desc = "Italic selection",
+      },
+      {
+        "<leader>b",
+        'c**<C-r>"**<Esc>',
+        mode = "v",
+        desc = "Bold selection",
+      },
+      {
+        '<leader>"',
+        'c"<C-r>""<Esc>',
+        mode = "v",
+        desc = "Double-quote selection",
+      },
+      {
+        "<leader>'",
+        [[c'<C-r>"'<Esc>]],
+        mode = "v",
+        desc = "Single-quote selection",
+      },
+      {
+        "<leader>(",
+        'c(<C-r>")<Esc>',
+        mode = "v",
+        desc = "Parenthesize selection",
+      },
+      {
+        "<leader>{",
+        'c{<C-r>"}<Esc>',
+        mode = "v",
+        desc = "Brace selection",
+      },
+      {
+        "<leader>[",
+        'c[<C-r>"]<Esc>',
+        mode = "v",
+        desc = "Bracket selection",
+      },
+    },
+  },
+}
